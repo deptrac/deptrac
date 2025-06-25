82572d88b6de1bbd602a56bd2d1d55540c1e9fd1
Gennadi McKelvey <gennadijanzen@icloud.com>
2025-06-25 22:56:20 +0200

    fix build.yml

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -6,6 +6,6 @@ use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 $config = new Configuration();
 
 return $config
-    ->addPathToScan(__DIR__ . '/bin/deptrac', isDev: false)
+    ->addPathToScan(__DIR__ . '/deptrac', isDev: false)
     ->ignoreErrorsOnExtension('ext-dom', [ErrorType::SHADOW_DEPENDENCY]) // in composer "suggests"
     ->ignoreErrorsOnPath(__DIR__ . '/tests', [ErrorType::UNKNOWN_CLASS, ErrorType::UNKNOWN_FUNCTION]); // keep ability to test invalid symbols
