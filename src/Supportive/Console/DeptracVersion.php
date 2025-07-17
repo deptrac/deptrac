70504ab2c6516942885833652fa578c3666da100
Gennadi McKelvey <gennadijanzen@icloud.com>
2025-07-17 23:24:02 +0200

    fix order of function by visiblity

--- src/Supportive/Console/DeptracVersion.php
+++ src/Supportive/Console/DeptracVersion.php
@@ -18,14 +18,6 @@ final class DeptracVersion
     private const SYMFONY_MAGIC_CONST_UNKNOWN = 'UNKNOWN';
     private const DEPTRAC_VERSION = '@git-version@';
 
-    /**
-     * If building as a PHAR with Box, Box will replace the value of DEPTRAC_VERSION at build time.If building as a PHAR with Box, Box will replace the value of DEPTRAC_VERSION at build time.
-     */
-    private static function hasGitVersion(): bool
-    {
-        return self::DEPTRAC_VERSION !== '@git-version@';
-    }
-
     public static function get(): string
     {
         if (self::hasGitVersion()) {
@@ -46,5 +38,13 @@ final class DeptracVersion
         }
 
         return $installedVersion;
+    }
+
+    /**
+     * If building as a PHAR with Box, Box will replace the value of DEPTRAC_VERSION at build time.
+     */
+    private static function hasGitVersion(): bool
+    {
+        return self::DEPTRAC_VERSION !== '@git-version@';
     }
 }
