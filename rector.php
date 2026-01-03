63c8ae5c3c57d9792957b0dc25d55b53fbb7a307
Gennadi McKelvey <gennadijanzen@icloud.com>
2026-01-03 12:38:04 +0100

    init rector config

---
+++ rector.php
@@ -0,0 +1,28 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([__DIR__])
+    ->withCache('./.cache/rector/')
+    ->withSkip([
+        './phpstan-baseline.php',
+        './vendor',
+        './tools',
+        './docs',
+        './tests/*/Fixtures/*',
+    ])
+    // ->withPhpSets(php81: true)
+    ->withPhpLevel(1)
+    ->withPreparedSets(
+        // typeDeclarations: true,
+        // privatization: true,
+    )
+    ->withAttributesSets(
+        // symfony: true,
+        // phpunit: true,
+    )
+    ->withImportNames(importShortClasses: false)
+    ->withParallel()
+;
